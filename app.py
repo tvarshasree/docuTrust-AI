ade7360288e997a99e138a0b2c3b3a320e89df69
tvarshasree <tvarsha0810@gmail.com>
2026-07-21 15:15:40 +0530

    updates

--- app.py
+++ app.py
@@ -200,4 +200,5 @@ async def websocket_chat_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=False)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
